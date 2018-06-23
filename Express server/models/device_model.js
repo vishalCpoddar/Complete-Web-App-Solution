7fd9c2072dd6f3d3465977883320883666816ad1
Vishal C. Poddar <mailtovcpoddar@gmail.com>
2018-06-23 23:55:42 +0530

    model_device_error_fixed

--- Express server/models/device_model.js
+++ Express server/models/device_model.js
@@ -22,19 +22,19 @@ const DeviceSchema = {
 
 const DEVICE = module.exports = mongoose.model('DEVICE', DeviceSchema);
 
-module.exports = findDeviceByDevId = function (dev, callback) {
+module.exports.findDeviceByDevId = function (dev, callback) {
     DEVICE.findById(dev.id, callback)
 };
 
 
-module.exports = findDeviceByUserId = function (user, callback) {
+module.exports.findDeviceByUserId = function (user, callback) {
     query = {
         userID: user.id
     }
     DEVICE.findOne(query, callback)
 };
 
-module.exports = updateDevice = function (device, callback) {
+module.exports.updateDevice = function (device, callback) {
     query = {
         _id: device._id,
         controls: device.controls,
@@ -44,15 +44,15 @@ module.exports = updateDevice = function (device, callback) {
 };
 
 
-module.exports = removeDevice = function (device, callback) {
+module.exports.removeDevice = function (device, callback) {
     DEVICE.findByIdAndRemove(device.id, callback);
 };
 
-module.exports=getAllDevice=function(callback){
+module.exports.getAllDevice=function(callback){
     DEVICE.find(callback);
 };
 
-module.exports=addDevice=function(device,callback){
+module.exports.addDevice=function(device,callback){
     DEVICE.create(device,callback);
 };
 
